da1d8c75febec060302d55e425f2afbcc1bd6f32
dasta <arzymatodastan@gmail.com>
2022-04-30 17:01:49 +0600

    Homework-10!

---
+++ src/tapshyrma10.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class tapshyrma10 {
+    // public static int[]array=new int[10];
+    //public static int []getArray1= get(array);
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        get(scanner.nextInt(), 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+    public static boolean get(int a, int... array) {
+        for (int i = 0; i < array.length; i++) {
+            if (a == array[i]) {
+                System.out.println(true + " " + a);
+                break;
+            } else {
+                System.out.println(false + " " + a);
+                break;
+            }
+        }
+        return true;
+    }
+}
